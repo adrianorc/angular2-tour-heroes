e98816e09ca6f2b0431661ea2526b719e5d6922f
Adriano Rodrigues da Cruz <adrianorc@outlook.com>
2016-09-16 14:41:51 -0300

    initial commit

---
+++ app/mock-heroes.js
@@ -0,0 +1,10 @@
+"use strict";
+exports.HEROES = [
+    { id: 10, name: 'Celeritas' },
+    { id: 11, name: 'Tronado' },
+    { id: 12, name: 'Dr IQ' },
+    { id: 13, name: 'Mr. Nice' },
+    { id: 14, name: 'Narco' },
+    { id: 15, name: 'W.W.' },
+];
+//# sourceMappingURL=mock-heroes.js.map
